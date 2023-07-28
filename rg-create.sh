1b3381d5044e9b507dfd7c14b00406b9d813e1da
Yannick Djomo <67871275+franckya@users.noreply.github.com>
2023-07-28 11:52:32 -0700

    Update rg-create.sh

--- rg-create.sh
+++ rg-create.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
 
-# Set the variables
-resource_group="YanSentinelRG"
-workspace_name="YaSentinelWK"
-subscription_name="Pay-As-You-Go"
+# Set the names of the resource groups
+resource_groups=("ResourceGroup1" "ResourceGroup2" "ResourceGroup3")
+
+# Set the Azure region where the resource groups will be created
 region="East US"
-retention_days=30
 
-# Set the active subscription
-az account set --subscription "$subscription_name"
-
-# Check if the Log Analytics workspace already exists
-if az monitor log-analytics workspace show --resource-group "$resource_group" --workspace-name "$workspace_name" &>/dev/null; then
-    echo "Log Analytics workspace '$workspace_name' already exists in the resource group '$resource_group'. Skipping creation."
-else
-    # Create the Log Analytics workspace
-    az monitor log-analytics workspace create --resource-group "$resource_group" --workspace-name "$workspace_name" --sku PerGB2018 --retention-time "$retention_days"
-    echo "Log Analytics workspace '$workspace_name' created successfully in the resource group '$resource_group'."
-fi
-
-# Get the Customer ID of the workspace
-customer_id=$(az monitor log-analytics workspace show --resource-group "$resource_group" --workspace-name "$workspace_name" --query properties.customerId -o tsv)
-echo "Customer ID for the Log Analytics workspace: $customer_id"
-
-# Link the Log Analytics workspace to Azure Sentinel
-sentinel_workspace_link=$(az monitor sentinel workspace link show --resource-group "$resource_group" --workspace-name "$workspace_name" --query id -o tsv)
-
-if [ -z "$sentinel_workspace_link" ]; then
-    az monitor sentinel workspace link create --resource-group "$resource_group" --workspace-name "$workspace_name" --primary-workspace "$workspace_name"
-    echo "Log Analytics workspace '$workspace_name' linked to Azure Sentinel."
-else
-    echo "Log Analytics workspace '$workspace_name' is already linked to Azure Sentinel."
-fi
+# Loop through the array and create each resource group
+for rg in "${resource_groups[@]}"
+do
+    # Check if the resource group already exists
+    if az group show --name "$rg" &>/dev/null; then
+        echo "Resource group '$rg' already exists. Skipping creation."
+    else
+        # Create the resource group
+        az group create --name "$rg" --location "$region"
+        echo "Resource group '$rg' created successfully."
+    fi
+done
